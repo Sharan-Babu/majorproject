a130ac434a48871c9100d31c28109faf9d9ad8c5
Sharan Babu <50396375+Sharan-Babu@users.noreply.github.com>
2023-03-03 10:50:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pickle
+from newscatcherapi import NewsCatcherApiClient
+from sentence_transformers import SentenceTransformer, util
+
+
+
+st.title('Gemini 👥')
+st.caption('Eclectic News Search Engine')
+API_KEY = '3h7H4_yMu2KchsDvXCt2x5TZ4m002_G8myOud-tPjl8'
+newscatcherapi = NewsCatcherApiClient(x_api_key=API_KEY)
+  
+threshold = 0.75
+
+@st.cache_resource
+def load_model():
+	return SentenceTransformer('msmarco-distilbert-base-tas-b')
+
+
+search_term = st.text_input("Enter query","russia vs ukraine")
+
+if st.button("Search"):
+	embedder = load_model()
+	news_articles = newscatcherapi.get_search(q=search_term, lang='en', page_size=5)
+	news_articles = news_articles['articles']
+
+	anchor_vertices_list = []
+	children_news_info = []
+	sentences_list = []
+
+	for x in news_articles:
+		if len(anchor_vertices_list) == 0:
+			anchor_vertices_list.append(x)
+			children_news_info.append([])
+			sentences_list.append(x['summary'])
+		else:
+			main_article_summary = x['summary']
+			main_article_embedding = embedder.encode(main_article_summary)
+			remaining_articles_embeddings = embedder.encode(sentences_list)
+			cos_sim = util.cos_sim(main_article_embedding, remaining_articles_embeddings)[0]
+			closest_to = -1
+			sim = 0
+			index = 0
+			for value in cos_sim:
+				if float(value) > threshold and float(value) > sim:
+					closest_to = index
+					sim = float(value)
+				index += 1
+			
+			if closest_to == -1:
+				anchor_vertices_list.append(x)
+				children_news_info.append([])
+				sentences_list.append(x['summary'])
+			else:
+				children_news_info[closest_to].append(x)
+							
+
+
+
+	#main_article_embedding = embedder.encode(main_article_summary)
+	#remaining_articles_embeddings = embedder.encode(remaining_articles_array)
+	#cos_sim = util.cos_sim(main_article_embedding, remaining_articles_embeddings)
+	#cos_sim[0] -- len(99)  # illustrates similarty between first news article and remaining 99 news articles in our dataset
+
+
+	# with open('anchor_vertices_list','rb') as file:
+	#     anchor_vertices_list = pickle.load(file)
+
+	# with open('children_news_info','rb') as file:
+	#     children_news_info = pickle.load(file)    
+
+	st.write(anchor_vertices_list)
+	st.write(children_news_info)
+
+
+
+	# Create 2 columns view
+	st.markdown("# Anchor News")
+
+	for i,article in enumerate(anchor_vertices_list):
+		link = article['link']
+		media = article['media']
+		rank = article['rank']
+		summary = article['summary']
+		title = article['title']
+		children = children_news_info[i]
+
+		st.subheader(f"{i+1}. {title}")
+		st.write(f"News Link: {link}")
+		st.image(media,caption="News Thumbnail")
+		st.markdown("#### Summary")
+		st.write(summary)
+		
+		if len(children)>0:
+			st.markdown("**Related Articles (ID)**:")
+			for related_article in children:
+				#st.markdown(f": ")
+				st.markdown(f"**{children_news_info[related_article]['title']}**: {related_article}")
+
+		st.write("")
+		st.write("")	
+		st.markdown("<hr>",unsafe_allow_html=True)
+
+
+	st.title("Search Related News")
+	related_news_id = st.text_input("News ID","8ee07b4cdb24bf72d877bb0834132495")
+	st.write("")
+
+	if st.button("Search"):
+		st.write("")
+		st.write("")
+		article = children_news_info[related_news_id]
+		link = article['link']
+		media = article['media']
+		rank = article['rank']
+		summary = article['summary']
+		title = article['title']
+
+		st.subheader(f"{title}")
+		st.write(f"News Link: {link}")
+		st.image(media)
+		st.markdown("#### Summary")
+		st.write(summary)
